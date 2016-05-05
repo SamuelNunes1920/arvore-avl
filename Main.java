6868d288ebdffd40e3f9f8d2ce560d21f3cbfa63
Joahannes Bruno <joahannes@gmail.com>
2016-05-05 06:27:52 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ public class Main {
 		System.out.print("Numero de elementos que deseja inserir: ");
 		int elementos = entrada.nextInt();
 		for(int i = 1; i <=elementos;i++){
-		  System.out.print(i + "° Elemento: ");
+		  System.out.print(i + "a Chave: ");
 		  int chave = entrada.nextInt();
 		  tree.insert(chave);
 		System.out.println("\nÁrvore montada: \n");		  
@@ -27,7 +27,7 @@ public class Main {
 		}
 
 		//Busca
-		System.out.print("\n*---------- BUSCA DE CHAVE ----------*");
+		System.out.print("*-------- BUSCA DE CHAVE -------------*");
 		System.out.print("\nBuscar a chave: ");
 		int buscaChave = entrada.nextInt();
 		tree.search(buscaChave);
@@ -35,12 +35,35 @@ public class Main {
 		System.out.print("\nBuscar a chave: ");
 		int buscaChave1 = entrada.nextInt();
 		tree.search(buscaChave1);
+		System.out.println("\nÁrvore montada: \n");		  
+		tree.displayTree();
 		
 		//Remocao
-		System.out.print("\n*-------- REMOVENDO CHAVE --------*\n");
+		System.out.print("\n*-------- REMOÇÃO DE CHAVE ----------*");
 		System.out.print("\nRemover a chave: ");
 		int removeChave = entrada.nextInt();
 		tree.remove(removeChave);
+		System.out.println("\nÁrvore montada: \n");
 		tree.displayTree();
+
+		//Busca
+		System.out.print("\n*-------- BUSCA DE CHAVE -------------*");
+		System.out.print("\nBuscar a chave: ");
+		int buscaChave2 = entrada.nextInt();
+		tree.search(buscaChave2);
+
+		//Inserção
+		System.out.print("\n*-------- INSERÇÃO DE CHAVE ----------*");
+		System.out.print("\nInserir chave: ");
+		int chave1 = entrada.nextInt();
+		tree.insert(chave1);
+		System.out.println("\nÁrvore montada: \n");		  
+		tree.displayTree();
+		System.out.print("\n");
+
+		//BuscaMaior		
+		System.out.println("\nBusca do maior: \n");		  
+		tree.buscaMaior(tree.root);
+		System.out.print("\n");
 	}
 }
